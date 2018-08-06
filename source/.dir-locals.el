90951dc3546883877360ba49d2e6561478a7aa88
vipererer <imxiyxz@gmail.com>
2018-08-06 13:14:14 +0800

    fix theme

--- source/.dir-locals.el
+++ source/.dir-locals.el
@@ -1,13 +1,11 @@
 ((nil
   (org-html-head-include-default-style . nil)
   (org-html-htmlize-output-type . css)
-  (org-html-head . "<link rel='stylesheet' href='https://www.pirilampo.org/styles/readtheorg/css/htmlize.css' />
-<link rel='stylesheet' href='https://www.pirilampo.org/styles/readtheorg/css/readtheorg.css' />
+  (org-html-head . "<link rel='stylesheet' href='/asset/common.css' />
 <script src='https://cdn.bootcss.com/jquery/2.1.3/jquery.min.js'></script>
 <script src='https://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
-<script src='https://www.pirilampo.org/styles/lib/js/jquery.stickytableheaders.min.js'></script>
-<script src='https://www.pirilampo.org/styles/readtheorg/js/readtheorg.js'></script>\n")
- (org-publish-project-alist
+<script src='/asset/common.js'></script>\n")
+  (org-publish-project-alist
    ("_notes"
     :base-directory "./"
     :base-extension "org"
@@ -19,7 +17,8 @@
     :with-toc 3
     :html-preamble t
     :auto-sitemap t
-    :sitemap-filename "index.org")
+    :author "某某人"
+    :sitemap-filename "sitemap.org")
    ("_assets"
     :base-directory "./"
     :base-extension "css\\|js\\|png\\|jpe?g\\|gif\\|svg\\|pdf\\|zip\\|html\\|txt"
