ccb475a0a28dc570d912d09af3d4f49d9083ba08
niu <teacher@g.com>
2018-09-13 07:26:38 +0000

    *Notes*.org

--- .source/Database/demo.sql
+++ .source/Database/demo.sql
@@ -1,46 +1,38 @@
-select * from lagou_position_result limit 5;
+-- 源数据
+select * from china_city limit 4;
+select * from lagou_position_bk limit 2; -- lagou_position_bk (工作信息，地区信息，分类信息，公司信息)
 
-create table area_demo1 (
-  area_id int auto_increment primary key,
-  sheng varchar(20) not null,
-  shi varchar(20) not null,
-  xian varchar(20) not null,
-  quhao varchar(20) not null,
-  youbian varchar(20),
-  jingdu decimal(10, 10),
-  weidu decimal(10, 10)
-  );
 
-insert into area_demo1 (sheng, shi, xian, quhao) values
-  ('广东省', '珠海市', '斗门区', '22323'),
-  ('广东省', '珠海市', '香洲区', '22324'),
-  ('广东省', '珠海市', '金湾区', '22325'),
-  ('广东省', '广州市', '天河区', '11155'),
-  ('广东省', '广州市', '越秀区', '11156'),
-  ('广东省', '广州市', '白云区', '11157');
+-- 要求，创建三张表
+-- lagou_city (cityid, province, city, district)  # 全国的省市县信息，需要从 china_city 表中提取出来
+-- lagou_company (cid, name, short_name, size, financestage) # 所有的公司表，从 lagou_position_bk 中分离出来
+-- lagou_position (pid, cityid, cid, position, field, salary_min, salary_max, workyear, education, ptype, pnature, advantage, published_at, updated_at)
 
-select * from area_demo1 where shi='珠海市' or shi='广州市';
 
-select distinct shi from area_demo1 where sheng='广东省';
+-- lagou_city 创建过程
+-- 373 市 3341 县 总和 3714
+select count(*) from china_city where depth=3;
 
-select * from area_demo1;
+create table lagou_city01 as
+select d.id, p.cityName as province, c.cityName as city, d.cityName as district from
+  (select * from china_city where depth=3) d
+    join china_city c on d.parentId = c.id and c.depth=2
+    join china_city p on c.parentId = p.id and p.depth=1;
 
-create table s_sheng (id int auto_increment primary key, name varchar(20));
-insert into s_sheng (name) values ('广东省'), ('山东省'), ('广西省'), ('湖南省');
+select * from lagou_city01 limit 1;
 
-create table s_shi (id int auto_increment primary key, name varchar(20), sheng int references s_sheng(id));
-insert into s_shi (name, sheng) values
-  ('珠海市', 1), ('广州市', 1), ('湛江', 1),
-  ('桂林市', 3), ('南宁市', 3), ('柳州', 3);
+insert into lagou_city01
+select c.id, p.cityName as province, c.cityName as city, null as district from (select * from china_city where depth=2) c
+  join china_city p on c.parentId = p.id and p.depth = 1;
 
-drop table if exists s_xian;
-create table s_xian (id int auto_increment primary key, name varchar(20), shi int references s_shi(id), youbian varchar(20), jingdu varchar(20), weidu  varchar(20));
-insert into s_xian (name, shi) values
-  ('斗门区',  1), ('金湾区', 1), ('香洲区', 1);
 
-select * from s_sheng;
-select * from s_shi;
-select * from s_xian;
+select count(*) from lagou_city01;
 
--- 全连接，笛卡尔积
-select * from s_shi full join s_sheng;
+create table lagou_city as
+select d.id, p.cityName as province, c.cityName as city, d.cityName as district from
+  (select * from china_city where depth=3) d
+    join china_city c on d.parentId = c.id and c.depth=2
+    join china_city p on c.parentId = p.id and p.depth=1
+union
+select c.id, p.cityName as province, c.cityName as city, null as district from (select * from china_city where depth=2) c
+  join china_city p on c.parentId = p.id and p.depth = 1;
